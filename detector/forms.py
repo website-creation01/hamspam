3cfbe840c2f70bdcec47307b333629c0ebc9fdf6
website-creation01 <swathikamadati5@gmail.com>
2025-04-17 22:27:20 +0530

    Add files via upload

---
+++ detector/forms.py
@@ -0,0 +1,5 @@
+from django import forms 
+
+class MessageForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your message here...'}))
+    
